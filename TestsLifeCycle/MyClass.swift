fb493f183bbf8fc43e7623424669b4ef84bc4e39
Edwin Cardenas <eacardenase@gmail.com>
2026-06-03 17:25:37 -0500

    feat: add initial test and check suite infrastructure

---
+++ TestsLifeCycle/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  TestsLifeCycle
+//
+//  Created by Edwin Cardenas on 6/3/26.
+//
+
+import Foundation
+
+class MyClass {
+    func methodOne() {
+        print(">> \(#function)")
+    }
+
+    func methodTwo() {
+        print(">> \(#function)")
+    }
+}
---
+++ TestsLifeCycleTests/MyClassTests.swift
@@ -0,0 +1,14 @@
+//
+//  MyClassTests.swift
+//  TestsLifeCycleTests
+//
+//  Created by Edwin Cardenas on 6/3/26.
+//
+
+import XCTest
+
+class MyClassTests: XCTestCase {
+    func test_zero() {
+        XCTFail("Tests not yet implemented in MyClassTests")
+    }
+}
