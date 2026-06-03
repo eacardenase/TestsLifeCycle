4708c2e71c9303a9bf69ae9f50820d1e00fc8240
Edwin Cardenas <eacardenase@gmail.com>
2026-06-03 17:49:52 -0500

    feat: basic tests with logs

--- TestsLifeCycleTests/MyClassTests.swift
+++ TestsLifeCycleTests/MyClassTests.swift
@@ -7,8 +7,18 @@
 
 import XCTest
 
+@testable import TestsLifeCycle
+
 class MyClassTests: XCTestCase {
-    func test_zero() {
-        XCTFail("Tests not yet implemented in MyClassTests")
+    func test_methodOne() {
+        let sut = MyClass()
+
+        sut.methodOne()
+    }
+
+    func test_methodTwo() {
+        let sut = MyClass()
+
+        sut.methodTwo()
     }
 }
