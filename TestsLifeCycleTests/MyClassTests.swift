3afe678a920642db9c3d30a2ed2edc7a1fc96896
Edwin Cardenas <eacardenase@gmail.com>
2026-06-03 18:36:05 -0500

    feat: use setUp and tearDown to manage duplicate code in tests cases

--- TestsLifeCycleTests/MyClassTests.swift
+++ TestsLifeCycleTests/MyClassTests.swift
@@ -10,7 +10,19 @@ import XCTest
 @testable import TestsLifeCycle
 
 class MyClassTests: XCTestCase {
-    private let sut = MyClass()
+    private var sut: MyClass!
+
+    override func setUp() {
+        super.setUp()
+
+        sut = MyClass()
+    }
+
+    override func tearDown() {
+        sut = nil
+
+        super.tearDown()
+    }
 
     func test_methodOne() {
         sut.methodOne()
